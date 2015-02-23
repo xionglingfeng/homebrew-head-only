59e82c675191e318824766a98f4708450e1f9181
Tim D. Smith <git@tim-smith.us>
2015-02-22 21:33:21 -0800

    squishyball (new formula)
    Submitted by an anonymous IRC user. Would need a FLAC fixture and
    terminal interaction to do a better test.
    
    Closes #94.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ squishyball.rb
@@ -0,0 +1,28 @@
+class Squishyball < Formula
+  homepage "https://xiph.org/"
+  head "http://svn.xiph.org/trunk/squishyball"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "libvorbis"
+  depends_on "opusfile"
+  depends_on "libao"
+  depends_on "flac"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    man1.install "squishyball.1"
+  end
+
+  test do
+    system bin/"squishyball", "-V"
+  end
+end
