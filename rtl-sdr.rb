159efc8294f12a839bb53304995888448994b2e6
Tim D. Smith <git@tim-smith.us>
2015-06-04 12:17:35 -0700

    rtl-sdr: add pkg-config dependency

--- rtl-sdr.rb
+++ rtl-sdr.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class RtlSdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/rtl-sdr'
   head 'git://git.osmocom.org/rtl-sdr.git'
 
   depends_on 'cmake' => :build
+  depends_on "pkg-config" => :build
   depends_on 'libusb'
 
   def install
