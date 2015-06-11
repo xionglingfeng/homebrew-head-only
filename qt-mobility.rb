96617ce5b320ed4da31881430c127916eed8c87e
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-11 06:15:54 +0100

    qt-mobility: fix url

--- qt-mobility.rb
+++ qt-mobility.rb
@@ -1,19 +1,17 @@
-require 'formula'
-
 class QtMobility < Formula
-  homepage 'http://qt.nokia.com/products/qt-addons/mobility'
-  head 'git://gitorious.org/qt-mobility/qt-mobility.git'
+  homepage "https://code.qt.io/cgit/qt-mobility/qt-mobility.git/"
+  head "https://code.qt.io/qt-mobility/qt-mobility.git"
 
-  depends_on 'qt'
+  depends_on "qt"
 
   def install
     system "./configure", "-release", "-prefix", prefix, "-qmake-exec", "#{HOMEBREW_PREFIX}/bin/qmake"
     system "make"
     ENV.j1
-    system "make install"
+    system "make", "install"
 
     # Move all .apps out of the "bin/" and into the prefix (like qt formula)
-    Pathname.glob(bin + '*.app').each do |path|
+    Pathname.glob(bin + "*.app").each do |path|
       mv path, prefix
     end
   end
