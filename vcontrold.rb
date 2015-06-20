8717dc91cde4f5f8fa2416ad0a4806f7f02db077
Markus Reiter <me@reitermark.us>
2015-06-20 03:58:39 +0200

    vcontrold (new formula)
    Closes #138.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ vcontrold.rb
@@ -0,0 +1,23 @@
+class Vcontrold < Formula
+  desc "Control and log Viessmann boilers."
+  homepage "https://openv.wikispaces.com/vcontrold"
+  head "http://svn.code.sf.net/p/vcontrold/code/trunk/vcontrold/"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libxml2"
+
+  def install
+    chmod 0755, "auto-build.sh"
+    system "./auto-build.sh"
+
+    system "./configure", "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+
+    system "make", "install"
+  end
+  test do
+    assert_match /usage/, shell_output("#{sbin}/vcontrold --help", 1)
+    assert_match /usage/, shell_output("#{bin}/vclient --help", 1)
+  end
+end
