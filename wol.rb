4fa01039aef7bc9fc37d5ef492d6bc4bfd079b54
Larry Gilbert <larry@l2g.to>
2015-06-24 14:13:28 -0700

    wol: more appropriate homepage; brew audit --strict
    Closes #120.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- wol.rb
+++ wol.rb
@@ -1,11 +1,14 @@
-require 'formula'
-
 class Wol < Formula
-  head 'https://github.com/kylef/wol.git'
-  homepage 'http://kylefuller.co.uk/'
+  desc "Small C application to wake a system via Wake-on-LAN"
+  homepage "https://github.com/kylef/wol"
+  head "https://github.com/kylef/wol.git"
 
   def install
     system "make"
     bin.install "bin/wol"
+  end
+
+  test do
+    system "wol", "00:00:00:00:00:00"
   end
 end
