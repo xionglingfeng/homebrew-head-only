c96a829cc4765efcb206067ffe3da9897a2d952f
George Macon <george.macon@gtri.gatech.edu>
2015-09-07 09:20:27 -0400

    agedu: build from git repository
    Closes #165.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- agedu.rb
+++ agedu.rb
@@ -4,9 +4,14 @@ class Agedu < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/'
 
   # The tarball here is not stable, and the checksum changes over time
-  head "http://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-r10126.tar.gz"
+  head "git://git.tartarus.org/simon/agedu.git"
+
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'halibut' => :build
 
   def install
+    system "./mkauto.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
