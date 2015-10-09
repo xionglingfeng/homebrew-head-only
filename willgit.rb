a3ce124eb8e85313cf8b0a1e002282f7e04d88c0
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-09 16:16:45 +0200

    willgit: use a mirror for HEAD
    Closes #163.
    
    Closes #171.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- willgit.rb
+++ willgit.rb
@@ -1,7 +1,9 @@
 class Willgit < Formula
   desc "William's miscellaneous git tools"
   homepage "http://git-wt-commit.rubyforge.org"
-  head "https://gitorious.org/willgit/mainline.git"
+  # This is a mirror; the original URL is down:
+  # https://gitorious.org/willgit/mainline.git
+  head "https://github.com/DanielVartanov/willgit.git"
 
   def install
     prefix.install "bin"
